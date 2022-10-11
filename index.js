b6cf164853c2e8305d4857612c4b231edd623a17
JediMasterJune <junmo2011@gmail.com>
2022-10-11 23:28:37 +0900

    Add pug

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 4000;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-
-    res.end('<h1>Hello World</h1>');
-    // 본문
+const { response } = require('express');
+const express = require('express');
+const port = 3000;
+const app = express();
+app.set('views', `${__dirname}/views`);
+//view engine을 사용하여 생성되는 템플릿 파일 저장 위치
+app.set('view engine', 'pug');
+app.get('/', (req, res) => {
+    return res.render('index.pug') // views 밑 상대 경로
 });
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-}) // 서버 실행
+app.get('/page', (req, res) => {
+    const { page } = req.query;
+    return res.render('board.pug', { page: page, });
+});
+app.get('/posts', (req, res) => {});
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
---
+++ index_http.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 4000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+    res.end('<h1>Hello World</h1>');
+    // 본문
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+}) // 서버 실행
